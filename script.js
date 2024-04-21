bac7b2c8f9402643f937986c1f27897c626bde10
David Anica <106039559+DJ-Anica@users.noreply.github.com>
2024-04-20 21:57:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Get the header element
+    var header = document.getElementById("header");
+
+    // Add a class to show the header with a fade-in effect
+    header.classList.add("fade-in-header");
+
+    // Wait for a very short moment, then remove the opacity to start the animation
+    setTimeout(function() {
+        header.style.opacity = 1;
+    }, 100);
+});
